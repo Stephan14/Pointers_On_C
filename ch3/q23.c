8dca85a3a38494ce573062f382045f35ff779e3a
zhenglikongming14 <1129930958@qq.com>
2016-01-15 15:36:51 +0800

    完成问题23

---
+++ ch3/q23.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+static char b = 2;
+void y( void )
+{
+}
+int a = 1;
+void x( void )
+{
+	int  c = 3;
+	static float d = 4;
+}
+int main(int argc, char *argv[])
+{
+
+	return 0;
+}
