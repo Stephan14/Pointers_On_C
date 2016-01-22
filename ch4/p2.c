8a8fb4e536c782860c3830ce635303ef0dbbcabb
zhenglikongming14 <1129930958@qq.com>
2016-01-22 14:14:02 +0800

    优化程序

--- ch4/p2.c
+++ ch4/p2.c
@@ -1,12 +1,14 @@
 #include<stdio.h>
 #include<math.h>
-
+/*
+  该方法对1和2两个数数不适用
+*/
 int isPrime( int n )
 {
   int time = (int)(sqrt(n)+1);
-  int index = 2;
+  int index = 3;//从奇数开始计算，每次都加2
 
-  for(; index < time; index++ )
+  for(; index < time; index = index + 2 )
   {
       if (n % index == 0) {
         return 0;
@@ -16,8 +18,10 @@ int isPrime( int n )
   return 1;
 }
 int main(int argc, char const *argv[]) {
-  int i = 2;
-  int num = 1;
+  int i = 3;
+  int num = 2;
+
+  printf("2\t");
 
   while (i < 101) {
 
@@ -31,7 +35,7 @@ int main(int argc, char const *argv[]) {
         printf("%d\n", i);
         num++;
       }
-    i++;
+    i = i + 2;
   }
   return 0;
 }
