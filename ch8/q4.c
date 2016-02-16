57a55eafeebd6e876504cedfe89c203ea44e2d90
Stephan14 <zsf657135507@gmail.com>
2016-02-16 13:14:12 +0800

    问题4答案

--- ch8/q4.c
+++ ch8/q4.c
@@ -4,10 +4,8 @@ char *front, *rear;
 front = &buffer[0];
 rear = &buffer[SIZE] - 1;
 while ( front < rear ) {
-  if( *front != *rear )
+  if( *front++ != *rear++ )
     break;
-  front++;
-  rear--;
 }
 if ( front >= rear ) {
   printf("It is a palindrome\n" );
