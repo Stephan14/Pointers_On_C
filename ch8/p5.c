9d80e63a6041601975f22332ff909ecc64f6a688
Stephan14 <zsf657135507@gmail.com>
2016-02-17 21:38:32 +0800

    编程练习5答案

--- ch8/p5.c
+++ ch8/p5.c
@@ -21,6 +21,38 @@ void matrix_multiply( int *m1, int *m2, int *r, int x, int y, int z)
     }
   }
 }
+
+
+/*答案代码如下，通过指针进行访问*/
+
+void matrix_multiply( int *m1, int *m2, int *r, int x, int y, int z )
+{
+  register int *m1p;
+  register int *m2p;
+  register int k;
+  int row;
+  int column;
+
+  for( row = 0; row < x; row++ )
+  {
+    for( column = 0; column < z; column++ )
+    {
+      m1p = m1 + row * y;
+      m2p = m2 + column;
+      *r = 0;
+
+      for( k = 0; k < y; k++ )
+      {
+        *r += *m1p * *m2p;
+        m1p += 1;
+        m2p += z;
+      }
+
+      r++;
+    }
+  }
+
+}
 int main(int argc, char const *argv[]) {
   int m1[] = { 2, -6, 3, 5, 1, -1};
   int m2[] = { 4, -2, -4, -5, -7, -3, 6, 7};
