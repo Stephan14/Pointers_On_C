0a708c9f61f9eb042a11f7d857f4a3e1a3ee0c5d
Stephan14 <zsf657135507@gmail.com>
2016-02-21 17:33:11 +0800

    编程练习4答案

---
+++ ch9/p4.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#define NUL '\0'
+
+size_t my_strnlen( char const *string , int size )
+{
+  int length = 0;
+  for( ; length <= size; length++ )
+    if ( *string++ == NUL ) break;
+  return length;
+}
+
+int my_strcat( char *dst, char const *src, int size )
+{
+  int length = size-- - (int) my_strnlen( src, size );
+  if ( length > 0 ) {
+    strncpy( dst, src, length );
+    dst[length] = NUL;
+  }
+}
+
+
+int main(int argc, char const *argv[]) {
+
+  return 0;
+}
