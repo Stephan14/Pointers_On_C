c1e77d73f3f070481cd2babd7cb1610d66e03211
Stephan14 <zsf657135507@gmail.com>
2016-02-23 09:58:16 +0800

    编程练习4答案

--- ch9/p4.c
+++ ch9/p4.c
@@ -12,8 +12,9 @@ size_t my_strnlen( char const *string , int size )
 int my_strcat( char *dst, char const *src, int size )
 {
   int length = size-- - (int) my_strnlen( src, size );
-  if ( length > 0 ) {
-    strncpy( dst, src, length );
+  if ( length > 0 )
+  {
+    strncat( dst, src, length );
     dst[length] = NUL;
   }
 }
