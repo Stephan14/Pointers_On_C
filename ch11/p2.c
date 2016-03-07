78ec1ecc749062269cf769875b60d90fad6f5538
Stephan14 <zsf657135507@gmail.com>
2016-03-07 23:40:45 +0800

    编程练习2答案

--- ch11/p2.c
+++ ch11/p2.c
@@ -2,18 +2,41 @@
 #include<stdlib.h>
 
 int main(int argc, char const *argv[]) {
-  int n;
+  int n, count = 0;
   int *array, *ptr;
 
   scanf("%d", &n);
   array = malloc( (n+1) * sizeof( int ) );
+  if( array == NULL )
+  {
+    printf(" malloc error\n" );
+    return ;
+  }
+
   ptr = array;
+  *ptr++ = n;
+  count++;
 
-  while( scanf("%d", ptr++ ) != EOF);
-
+  while( scanf("%d", ptr++ ) != EOF)
+  {
+    count++;
+    if( count >= n )
+    {
+      array = realloc( array,  (n = 2 * n + 1) );
+      if( array == NULL )
+      {
+        printf( " realloc errror \n" );
+        return ;
+      }
+      else
+        ptr = array + n / 2;
+    }
+  }
   int index;
-  for( index = 0; index < n; index++ )
-    printf("%d\n", array[index] );
-    
+  for( index = 0; index < count ;index++ )
+  {
+    printf(" %d \n", array[index]);
+  }
+
   return 0;
 }
