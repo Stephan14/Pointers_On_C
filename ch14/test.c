871034365d24055b02af094cac3499c532a12927
stephan14 <zsf657135507@gmail.com>
2016-12-14 23:14:52 +0800

    测试宏

---
+++ ch14/test.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+//#的作用
+#define PRINT(FORMAT, VLALUE)  \
+        printf( "The value is " FORMAT "\n", VLALUE )
+//##的作用
+#define ADD_TO_SUM( sum_number, value  )    \
+        sum ## sum_number += value
+//宏参数的副作用,多次执行同一个命令
+#define MAX( a, b )  ( (a) > (b) ? (a) : (b) )
+
+
+int main( int argc, char *argv[] )
+{
+    int a = 3;
+    PRINT( "%d", a + 3 );
+    int sum5 = 5;
+    ADD_TO_SUM( 5, 25 );
+    printf( "sum5 = %d\n", sum5 );
+
+    int x = 5;
+    int y = 8;
+    int z = MAX( x++, y++ );
+    printf( " x = %d, y = %d, z = %d\n", x, y, z );
+
+    //测试字符串连接
+    printf("test1"
+           "test2\n");
+    printf("test3 \
+     test4");
+    return 0;
+} 
