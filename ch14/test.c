fea4dcca4e68fee03402832049d054b7a410ba93
stephan14 <zsf657135507@gmail.com>
2016-12-15 21:56:06 +0800

    增加预处理器符号的使用

--- ch14/test.c
+++ ch14/test.c
@@ -27,6 +27,12 @@ int main( int argc, char *argv[] )
     printf("test1"
            "test2\n");
     printf("test3 \
-     test4");
+     test4\n");
+
+    printf("__FILE__ : %s\n", __FILE__ );
+    printf("__DATE__ : %s\n", __DATE__ );
+    printf("__LINE__ : %d\n", __LINE__ );
+    printf("__TIME__ : %s\n", __TIME__ );
+    printf("__STDC__ : %d\n", __STDC__ );
     return 0;
 } 
